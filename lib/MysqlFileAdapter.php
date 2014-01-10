d2e99b70c3f49e308f38091f7b3ab4748c9fa264
Yani Iliev <yani@servmask.com>
2014-01-10 09:02:39 +0700

    Fixing indentation

--- lib/MysqlFileAdapter.php
+++ lib/MysqlFileAdapter.php
@@ -48,28 +48,28 @@
  */
 class MysqlFileAdapter
 {
-	protected $fileHandler = null;
+    protected $fileHandler = null;
 
-	public function open($fileName)
+    public function open($fileName)
     {
-		$this->fileHandler = fopen($fileName, 'wb');
-		if (false === $this->fileHandler) {
+        $this->fileHandler = fopen($fileName, 'wb');
+        if (false === $this->fileHandler) {
             throw new \Exception('Output file is not writable', 2);
-		}
-	}
+        }
+    }
 
-	public function write($str)
+    public function write($str)
     {
-		$bytesWritten = 0;
-		if (false === ($bytesWritten = fwrite($this->fileHandler, $str))) {
-			throw new \Exception('Writting to file failed! Probably, there is no more free space left?', 4);
-		}
+        $bytesWritten = 0;
+        if (false === ($bytesWritten = fwrite($this->fileHandler, $str))) {
+            throw new \Exception('Writting to file failed! Probably, there is no more free space left?', 4);
+        }
 
-		return $bytesWritten;
-	}
+        return $bytesWritten;
+    }
 
-	public function close()
+    public function close()
     {
-		return fclose($this->fileHandler);
-	}
+        return fclose($this->fileHandler);
+    }
 }
